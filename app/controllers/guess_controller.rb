eac6c8c455946f92a4454660da5f68dbd129d6aa
bethannezink <bethannezink@gmail.com>
2015-04-16 11:26:37 -0400

    going back

--- app/controllers/guess_controller.rb
+++ app/controllers/guess_controller.rb
@@ -1,10 +1,9 @@
 class GuessController < ApplicationController
-
   before_action :current_player 
 
   def index
-    if Song.find(:conditions => ["name LIKE ?", "%#{params[:guess]}%")  #Song.find_by("name LIKE ?", "%#{params[:guess]}%")
-      @guess = Song.find(:conditions => ["name LIKE ?", "%#{params[:guess]}%") 
+    if Song.find_by("name LIKE ?", "%#{params[:guess]}%") # Song.find(:conditions => ["name LIKE ?", "%#{params[:guess]}%])
+      @guess = Song.find_by("name LIKE ?", "%#{params[:guess]}%")
       @song = Song.find(params[:song_id])
       if @guess.id == @song.id
         current_player.add_point
