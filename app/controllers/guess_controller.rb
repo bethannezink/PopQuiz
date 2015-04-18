9c35aaa814f15a7366cd051ce0e540db777d9512
bethannezink <bethannezink@gmail.com>
2015-04-18 17:32:07 -0400

    half-fuzzy

--- app/controllers/guess_controller.rb
+++ app/controllers/guess_controller.rb
@@ -2,8 +2,8 @@ class GuessController < ApplicationController
   before_action :current_player 
 
   def index
-    if Song.find_by("name LIKE ?", "%#{params[:guess].strip}%") 
-      @guess = Song.find_by("name LIKE ?", "%#{params[:guess].strip}%")
+    if Song.find_by("name LIKE ?", "#{params[:guess].strip}%") 
+      @guess = Song.find_by("name LIKE ?", "#{params[:guess].strip}%")
       @song = Song.find(params[:song_id])
       if @guess.id == @song.id
         current_player.add_point
