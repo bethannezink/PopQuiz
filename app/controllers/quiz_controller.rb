a0563ee439b0c2f87d7484a4b41932e7a89fc2bb
bethannezink <bethannezink@gmail.com>
2015-04-19 19:16:38 -0400

    artist name added to match new model

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -5,6 +5,7 @@ before_action :current_player
   def index
     #will need to ensure that song has a preview_url -- in model, ActiveRecordQuerying - "SELECT * id FROM songs WHERE preview_url IS NOT NULL"
     @song = Song.find((1..201).to_a.sample)
+    @artist = @song.artist
     @album = @song.album
     
   end
