a58670a1ad716040b302314d591daac69e4e6ad2
bethannezink <bethannezink@gmail.com>
2015-04-19 19:13:37 -0400

    fixes some song issues

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,7 +4,7 @@ before_action :current_player
 
   def index
     #will need to ensure that song has a preview_url -- in model, ActiveRecordQuerying - "SELECT * id FROM songs WHERE preview_url IS NOT NULL"
-    @song = Song.find((252..452).to_a.sample)
+    @song = Song.find((1..201).to_a.sample)
     @album = @song.album
     
   end
