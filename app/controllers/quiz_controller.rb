89087206fbb93656d55c05d8aa2df84f02b345dd
bethannezink <bethannezink@gmail.com>
2015-04-19 19:37:45 -0400

    makes random song selection AWESOMELY better

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -4,10 +4,9 @@ before_action :current_player
 
   def index
     #will need to ensure that song has a preview_url -- in model, ActiveRecordQuerying - "SELECT * id FROM songs WHERE preview_url IS NOT NULL"
-    @song = Song.find((1..201).to_a.sample)
+    @song = Song.find((Song.where("preview_url IS NOT NULL")).sample)
     @artist = @song.artist
     @album = @song.album
-    
   end
 
 end
