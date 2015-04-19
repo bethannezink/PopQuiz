f2c229ecad49a87b01abe4ce25aaffcb1068109f
bethannezink <bethannezink@gmail.com>
2015-04-19 19:44:24 -0400

    better better search

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -3,8 +3,7 @@ class QuizController < ApplicationController
 before_action :current_player
 
   def index
-    #will need to ensure that song has a preview_url -- in model, ActiveRecordQuerying - "SELECT * id FROM songs WHERE preview_url IS NOT NULL"
-    @song = Song.find((Song.where("preview_url IS NOT NULL")).sample)
+    @song = Song.where("preview_url IS NOT NULL").sample
     @artist = @song.artist
     @album = @song.album
   end
